cbe95595887dd0783dcaf62353ccdd5bc7717df8
mariopino <info@quequiereshacer.es>
2020-10-12 13:57:01 +0200

    typos

--- components/Additional.vue
+++ components/Additional.vue
@@ -55,9 +55,19 @@
       est laborum.
     </p>
 
-    <h5 class="pt-4">
-      Key handling policies; Failover and or high availability approaches
-    </h5>
+    <h5 class="pt-4">Key handling policies</h5>
+    <hr />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+
+    <h5 class="pt-4">Failover and or high availability approaches</h5>
     <hr />
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -170,9 +180,7 @@
     <hr />
     <p>Yes</p>
 
-    <h5 class="pt-4">
-      Which regions do they operate in?
-    </h5>
+    <h5 class="pt-4">Which regions do they operate in?</h5>
     <hr />
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
