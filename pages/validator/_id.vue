6d19c98643911b324df8bdf0c730fa4fe6781d30
mariopino <info@quequiereshacer.es>
2020-10-12 13:19:48 +0200

    validator page

--- pages/validator/_id.vue
+++ pages/validator/_id.vue
@@ -15,102 +15,212 @@
         Validator {{ shortAddress(accountId) }}
       </h1>
       <!-- <pre class="text-white">{{ JSON.stringify(validator, null, 2) }}</pre> -->
-      <b-card-group deck>
-        <b-card bg-variant="dark" text-variant="white" title="Identity">
-          <div v-if="validator.identity.legal" class="row">
-            <div class="col-md-3 text-right">Legal name:</div>
-            <div class="col-md-9">
-              {{ validator.identity.legal }}
+      <b-tabs content-class="mt-3 pt-4">
+        <b-tab title="Chain data" active>
+          <div class="row">
+            <div class="col-md-6 mb-5">
+              <div class="row">
+                <div class="col-8">
+                  <h4 class="mb-0">Identity</h4>
+                </div>
+                <div class="col-4 text-right text-success">
+                  <font-awesome-icon
+                    icon="check"
+                    class="text-success verified"
+                  />
+                  Very good
+                </div>
+              </div>
+              <hr />
+              <div v-if="validator.identity.legal" class="row">
+                <div class="col-md-3">Legal name:</div>
+                <div class="col-md-9">
+                  {{ validator.identity.legal }}
+                </div>
+              </div>
+              <div v-if="validator.identity.email" class="row">
+                <div class="col-md-3">Email:</div>
+                <div class="col-md-9">
+                  <a
+                    :href="`mailto:${validator.identity.email}`"
+                    target="_blank"
+                  >
+                    {{ validator.identity.email }}
+                  </a>
+                </div>
+              </div>
+              <div v-if="validator.identity.web" class="row">
+                <div class="col-md-3">Web:</div>
+                <div class="col-md-9">
+                  <a :href="validator.identity.web" target="_blank">
+                    {{ validator.identity.web }}
+                  </a>
+                </div>
+              </div>
+              <div v-if="validator.identity.twitter" class="row">
+                <div class="col-md-3">Twitter:</div>
+                <div class="col-md-9">
+                  <a
+                    :href="`https://twitter.com/${validator.identity.twitter.substring(
+                      1
+                    )}`"
+                    target="_blank"
+                  >
+                    {{ validator.identity.twitter }}
+                  </a>
+                </div>
+              </div>
+              <div v-if="validator.identity.riot" class="row">
+                <div class="col-md-3">Element:</div>
+                <div class="col-md-9">
+                  <a href="https://app.element.io/" target="_blank">
+                    {{ validator.identity.riot }}
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 mb-5">
+              <div class="row">
+                <div class="col-9">
+                  <h4 class="mb-0">Address creation</h4>
+                </div>
+                <div class="col-3 text-right text-success">
+                  <font-awesome-icon
+                    icon="check"
+                    class="text-success verified"
+                  />
+                  Very good
+                </div>
+              </div>
+              <hr />
+              <p>Address was created at block #20,000 (2020-10-10 12:31:49)</p>
             </div>
           </div>
-          <div v-if="validator.identity.email" class="row">
-            <div class="col-md-3 text-right">Email:</div>
-            <div class="col-md-9">
-              <a :href="`mailto:${validator.identity.email}`" target="_blank">
-                {{ validator.identity.email }}
-              </a>
+          <div class="row">
+            <div class="col-md-6 mb-5">
+              <div class="row">
+                <div class="col-9">
+                  <h4 class="mb-0">Slashes</h4>
+                </div>
+                <div class="col-3 text-right text-success">
+                  <font-awesome-icon
+                    icon="check"
+                    class="text-success verified"
+                  />
+                  Good
+                </div>
+              </div>
+              <hr />
+              <p>Good! No slashes detected</p>
+            </div>
+            <div class="col-md-6 mb-5">
+              <div class="row">
+                <div class="col-9">
+                  <h4 class="mb-0">Subaccounts</h4>
+                </div>
+                <div class="col-3 text-right text-success">
+                  <font-awesome-icon
+                    icon="check"
+                    class="text-success verified"
+                  />
+                  Good
+                </div>
+              </div>
+              <hr />
+              <p>Good! Detected sub-identity</p>
             </div>
           </div>
-          <div v-if="validator.identity.web" class="row">
-            <div class="col-md-3 text-right">Web:</div>
-            <div class="col-md-9">
-              <a :href="validator.identity.web" target="_blank">
-                {{ validator.identity.web }}
-              </a>
+          <div class="row">
+            <div class="col-md-6 mb-5">
+              <div class="row">
+                <div class="col-9">
+                  <h4 class="mb-0">Nominators</h4>
+                </div>
+                <div class="col-3 text-right text-danger">
+                  <font-awesome-icon icon="minus-circle" class="verified" />
+                  Bad
+                </div>
+              </div>
+              <hr />
+              <p>Detected {{ validator.nominators }} nominators</p>
+            </div>
+            <div class="col-md-6 mb-5">
+              <div class="row">
+                <div class="col-9">
+                  <h4 class="mb-0">Era points</h4>
+                </div>
+                <div class="col-3 text-right text-success">
+                  <font-awesome-icon
+                    icon="check"
+                    class="text-success verified"
+                  />
+                  Good
+                </div>
+              </div>
+              <hr />
+              <p>On average</p>
             </div>
           </div>
-          <div v-if="validator.identity.twitter" class="row">
-            <div class="col-md-3 text-right">Twitter:</div>
-            <div class="col-md-9">
-              <a
-                :href="`https://twitter.com/${validator.identity.twitter.substring(
-                  1
-                )}`"
-                target="_blank"
-              >
-                {{ validator.identity.twitter }}
-              </a>
+          <div class="row">
+            <div class="col-md-6 mb-5">
+              <div class="row">
+                <div class="col-9">
+                  <h4 class="mb-0">Commission</h4>
+                </div>
+                <div class="col-3 text-right text-success">
+                  <font-awesome-icon
+                    icon="check"
+                    class="text-success verified"
+                  />
+                  Good
+                </div>
+              </div>
+              <hr />
+              <p>
+                Very Good! commission &lt; 10% but > 5% and decreased over time
+              </p>
+            </div>
+            <div class="col-md-6 mb-5">
+              <div class="row">
+                <div class="col-9">
+                  <h4 class="mb-0">Frequency of payouts</h4>
+                </div>
+                <div class="col-3 text-right text-success">
+                  <font-awesome-icon
+                    icon="check"
+                    class="text-success verified"
+                  />
+                  Good
+                </div>
+              </div>
+              <hr />
+              <p>Good! Last payout was yesterday</p>
             </div>
           </div>
-          <div v-if="validator.identity.riot" class="row">
-            <div class="col-md-3 text-right">Element:</div>
-            <div class="col-md-9">
-              <a href="https://app.element.io/" target="_blank">
-                {{ validator.identity.riot }}
-              </a>
+          <div class="row">
+            <div class="col-md-6 mb-5">
+              <div class="row">
+                <div class="col-9">
+                  <h4 class="mb-0">Governance participation</h4>
+                </div>
+                <div class="col-3 text-right text-success">
+                  <font-awesome-icon
+                    icon="check"
+                    class="text-success verified"
+                  />
+                  Good
+                </div>
+              </div>
+              <hr />
+              <p>Good! The validator is participating</p>
             </div>
+            <div class="col-md-6 mb-5"></div>
           </div>
-        </b-card>
-        <b-card bg-variant="dark" text-variant="white" title="Address creation">
-          <span class="rating">Very Good!</span>
-          <p>Address was created at block #20,000 (2020-10-10 12:31:49)</p>
-        </b-card>
-      </b-card-group>
-      <b-card-group deck class="mt-4">
-        <b-card bg-variant="dark" text-variant="white" title="Slashes">
-          <p>Good! No slashes detected</p>
-        </b-card>
-        <b-card bg-variant="dark" text-variant="white" title="Subaccounts">
-          <p>Good! Detected sub-identity</p>
-        </b-card>
-      </b-card-group>
-      <b-card-group deck class="mt-4">
-        <b-card
-          bg-variant="dark"
-          text-variant="white"
-          title="Number of nominators"
-        >
-          <p>Good! Detected {{ validator.nominators }} nominators</p>
-        </b-card>
-        <b-card
-          bg-variant="dark"
-          text-variant="white"
-          title="Average era points"
-        >
-          <p>Good! On average</p>
-        </b-card>
-      </b-card-group>
-      <b-card-group deck class="mt-4">
-        <b-card bg-variant="dark" text-variant="white" title="Commission">
-          <p>Very Good! commission &lt; 10% but > 5% and decreased over time</p>
-        </b-card>
-        <b-card
-          bg-variant="dark"
-          text-variant="white"
-          title="Frequency of payouts"
-        >
-          <p>Good! Last payout was yesterday</p>
-        </b-card>
-      </b-card-group>
-      <b-card-group deck class="mt-4">
-        <b-card
-          bg-variant="dark"
-          text-variant="white"
-          title="Governance participation"
-        >
-          <p>Good! The validator is participating</p>
-        </b-card>
-      </b-card-group>
+        </b-tab>
+        <b-tab title="Additional data">
+          <p>Additional data provided by the validator</p>
+        </b-tab>
+      </b-tabs>
     </div>
   </div>
 </template>
