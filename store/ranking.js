4dceb8617e23d2492fe9418a16d04a776d427a84
mariopino <info@quequiereshacer.es>
2020-10-18 17:34:57 +0200

    historic metrics

--- store/ranking.js
+++ store/ranking.js
@@ -2,13 +2,15 @@ import { ApiPromise, WsProvider } from '@polkadot/api'
 
 export const state = () => ({
   list: [],
+  eraHistory: [],
   loading: true,
 })
 
 export const mutations = {
-  update(state, ranking) {
+  update(state, ranking, eraHistory) {
     state.list = ranking
     state.loading = false
+    state.eraHistory = eraHistory
   },
 }
 
@@ -19,11 +21,10 @@ export const actions = {
     const wsProvider = new WsProvider(nodeWs)
     const api = await ApiPromise.create({ provider: wsProvider })
 
-    // number of eras to look for
-    const historySize = 5
+    const historySize = 28 // 1 week
     const withActive = false
     const erasHistoric = await api.derive.staking.erasHistoric(withActive)
-    const eraIndex = erasHistoric.slice(
+    const eraIndexes = erasHistoric.slice(
       Math.max(erasHistoric.length - historySize, 0)
     )
     // parallelize as much as possible
@@ -31,6 +32,7 @@ export const actions = {
       validatorAddresses,
       waitingInfo,
       nominators,
+      councilVotes,
       erasPoints,
       erasPreferences,
       erasSlashes,
@@ -38,9 +40,10 @@ export const actions = {
       api.query.session.validators(),
       api.derive.staking.waitingInfo(),
       api.query.staking.nominators.entries(),
-      api.derive.staking._erasPoints(eraIndex),
-      api.derive.staking._erasPrefs(eraIndex),
-      api.derive.staking._erasSlashes(eraIndex),
+      api.derive.council.votes(),
+      api.derive.staking._erasPoints(eraIndexes),
+      api.derive.staking._erasPrefs(eraIndexes),
+      api.derive.staking._erasSlashes(eraIndexes),
     ])
 
     //
@@ -69,13 +72,49 @@ export const actions = {
         judgements.some(
           ([, judgement]) => judgement.isKnownGood || judgement.isReasonable
         ) || false
+
       const slashed = erasSlashes.some(
         ({ validators }) => validators[validator.accountId]
       )
+
       const slashes =
         erasSlashes.filter(
           ({ validators }) => validators[validator.accountId]
         ) || []
+
+      const commissionHistory = []
+      erasPreferences.forEach(({ validators }) => {
+        if (validators[validator.accountId]) {
+          commissionHistory.push(
+            (validators[validator.accountId].commission / 10000000).toFixed(2)
+          )
+        } else {
+          commissionHistory.push(null)
+        }
+      })
+
+      const councilBacking = councilVotes.some(
+        (vote) => vote[0] === validator.accountId
+      )
+
+      const eraPointsHistory = []
+      erasPoints.forEach(({ validators }) => {
+        if (validators[validator.accountId]) {
+          eraPointsHistory.push(validators[validator.accountId])
+        } else {
+          eraPointsHistory.push(null)
+        }
+      })
+
+      // frecuency of payouts
+      const claimedRewards = JSON.parse(
+        JSON.stringify(validator.stakingLedger.claimedRewards)
+      )
+      const payoutHistory =
+        JSON.parse(JSON.stringify(eraIndexes)).map((eraIndex) =>
+          claimedRewards.some((claimedEra) => claimedEra === eraIndex)
+        ) || []
+
       validator = JSON.parse(JSON.stringify(validator))
       return {
         active: true,
@@ -84,9 +123,13 @@ export const actions = {
         identity: JSON.parse(JSON.stringify(validator.identity)),
         stashAddress: validator.accountId,
         nominators: validator.exposure.others.length,
-        commission: (validator.validatorPrefs.commission / 10000000).toFixed(0),
+        commission: (validator.validatorPrefs.commission / 10000000).toFixed(1),
+        commissionHistory,
+        eraPointsHistory,
         slashed,
         slashes,
+        councilBacking,
+        payoutHistory,
       }
     })
 
@@ -130,9 +173,44 @@ export const actions = {
       const slashed = erasSlashes.some(
         ({ validators }) => validators[intention.accountId]
       )
-      const slash =
-        erasSlashes.find(({ validators }) => validators[intention.accountId]) ||
-        {}
+      const slashes =
+        erasSlashes.filter(
+          ({ validators }) => validators[intention.accountId]
+        ) || []
+
+      const commissionHistory = []
+      erasPreferences.forEach(({ validators }) => {
+        if (validators[intention.accountId]) {
+          commissionHistory.push(
+            (validators[intention.accountId].commission / 10000000).toFixed(2)
+          )
+        } else {
+          commissionHistory.push(null)
+        }
+      })
+
+      const eraPointsHistory = []
+      erasPoints.forEach(({ validators }) => {
+        if (validators[intention.accountId]) {
+          eraPointsHistory.push(validators[intention.accountId])
+        } else {
+          eraPointsHistory.push(null)
+        }
+      })
+
+      const councilBacking = councilVotes.some(
+        (vote) => vote[0] === intention.accountId
+      )
+
+      // frecuency of payouts
+      const claimedRewards = JSON.parse(
+        JSON.stringify(intention.stakingLedger.claimedRewards)
+      )
+      const payoutHistory =
+        eraIndexes.map((eraIndex) =>
+          claimedRewards.some((claimedEra) => claimedEra === eraIndex)
+        ) || []
+
       return {
         active: false,
         name: getName(intention.identity),
@@ -141,8 +219,12 @@ export const actions = {
         stashAddress: intention.accountId,
         nominators: intention.stakers.length,
         commission: (intention.validatorPrefs.commission / 10000000).toFixed(0),
+        commissionHistory,
+        eraPointsHistory,
         slashed,
-        slash,
+        slashes,
+        councilBacking,
+        payoutHistory,
       }
     })
     const ranking = validators.concat(intentions).map((validator, index) => {
@@ -153,7 +235,7 @@ export const actions = {
     })
 
     console.log(JSON.parse(JSON.stringify(ranking)))
-    context.commit('update', ranking)
+    context.commit('update', ranking, eraIndexes)
     const endTime = new Date().getTime()
     // eslint-disable-next-line
     console.log(
