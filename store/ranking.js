b6622889aa97acbd2dc1c3813c9009394fabf960
mariopino <info@quequiereshacer.es>
2020-10-24 12:07:57 +0200

    typo

--- store/ranking.js
+++ store/ranking.js
@@ -456,7 +456,7 @@ export const actions = {
     )
     // eslint-disable-next-line
     console.log(
-      `total: ${((dataCollectionTime + dataProcessingTime) / 1000).toFixed(3)}s`
+      `total time: ${((dataCollectionTime + dataProcessingTime) / 1000).toFixed(3)}s`
     )
   },
 }
