0a9e65d24496d9444f4d524019f335595668a6b0
mariopino <info@quequiereshacer.es>
2020-10-29 21:50:56 +0100

    nominate tx work in progress

--- pages/nominate.vue
+++ pages/nominate.vue
@@ -1,52 +1,88 @@
 <template>
   <b-container class="py-5">
-    <h1 class="mb-4">Nominate selected</h1>
-    <b-alert v-if="!detectedExtension" variant="danger" show>
-      <i class="fa fa-frown-o"></i>
-      Extension not found!
-    </b-alert>
-    <b-alert v-if="noAccountsFound" variant="danger" show>
-      <i class="fa fa-frown-o"></i> No accounts found!
-    </b-alert>
-    <b-form class="mt-2" @submit="onSubmit">
-      <b-form-group
-        id="input-group-from"
-        label="Address"
-        label-for="input-from"
-        class="w-100"
+    <div v-if="loading">
+      <Loading />
+    </div>
+    <div v-else>
+      <h1 class="mb-4">Nominate selected</h1>
+      <h5>Your validator set:</h5>
+      <hr />
+      <div
+        v-for="validator in list"
+        :key="validator.stashAddress"
+        class="row pb-1"
       >
-        <b-form-select
-          id="input-from"
-          v-model="$v.selectedAddress.$model"
-          :options="extensionAddresses"
-          :state="validateState('selectedAddress')"
-          aria-describedby="selectedAddress-feedback"
-          class="w-100"
-          @change="getBalance(selectedAddress)"
-        ></b-form-select>
-        <div>
-          <p
-            v-if="tranferableBalance"
-            class="ml-2 mb-0 mt-1"
-            :class="{ 'text-danger': !(tranferableBalance > 0) }"
-          >
-            Transferable balance:
-            {{ formatAmount(tranferableBalance) }}
-          </p>
+        <div class="col-10">
+          <Identicon :address="validator.stashAddress" :size="20" />
+          <nuxt-link :to="`/validator/${validator.stashAddress}`">
+            <span v-if="validator.name">
+              {{ validator.name }}
+              <VerifiedIcon />
+            </span>
+            <span v-else>
+              {{ shortAddress(validator.stashAddress) }}
+            </span>
+          </nuxt-link>
         </div>
-        <b-form-invalid-feedback id="selectedAddress-feedback"
-          >Please install Polkadot JS extension
-        </b-form-invalid-feedback>
-      </b-form-group>
-      <b-button
-        type="submit"
-        variant="outline-kusama"
-        class="btn-block mt-3"
-        :disabled="noAccountsFound"
-      >
-        Nominate
-      </b-button>
-    </b-form>
+        <div class="col-2 text-right">
+          <a
+            v-b-tooltip.hover
+            href="#"
+            title="Remove"
+            class="remove"
+            @click.stop.prevent="remove(validator.stashAddress)"
+          >
+            <font-awesome-icon icon="times" />
+          </a>
+        </div>
+      </div>
+      <b-alert v-if="!detectedExtension" variant="danger" show>
+        <i class="fa fa-frown-o"></i>
+        Extension not found!
+      </b-alert>
+      <b-alert v-if="noAccountsFound" variant="danger" show>
+        <i class="fa fa-frown-o"></i> No accounts found!
+      </b-alert>
+      <b-form class="mt-2" @submit="onSubmit">
+        <b-form-group
+          id="input-group-from"
+          label="Select controller address"
+          label-for="input-from"
+          class="w-100 pt-4"
+        >
+          <b-form-select
+            id="input-from"
+            v-model="$v.selectedAddress.$model"
+            :options="extensionAddresses"
+            :state="validateState('selectedAddress')"
+            aria-describedby="selectedAddress-feedback"
+            class="w-100"
+            @change="getBalance(selectedAddress)"
+          ></b-form-select>
+          <div>
+            <p
+              v-if="tranferableBalance"
+              class="ml-2 mb-0 mt-1"
+              :class="{ 'text-danger': !(tranferableBalance > 0) }"
+            >
+              Transferable balance:
+              {{ formatAmount(tranferableBalance) }}
+            </p>
+          </div>
+          <b-form-invalid-feedback id="selectedAddress-feedback"
+            >Please install Polkadot JS extension
+          </b-form-invalid-feedback>
+        </b-form-group>
+        <b-button
+          type="submit"
+          variant="outline-kusama"
+          class="btn-block mt-3"
+          :disabled="noAccountsFound"
+        >
+          Nominate
+        </b-button>
+      </b-form>
+    </div>
   </b-container>
 </template>
 
@@ -96,22 +132,28 @@ export default {
       noAccountsFound: false,
     }
   },
+  computed: {
+    loading() {
+      return this.$store.state.ranking.loading
+    },
+    list() {
+      return this.$store.state.ranking.list.filter(({ stashAddress }) =>
+        this.selectedAddresses.includes(stashAddress)
+      )
+    },
+    selectedAddresses() {
+      return this.$store.state.ranking.selectedAddresses
+    },
+  },
   validations: {
     selectedAddress: {
       required,
     },
-    amount: {
-      required,
-      integer,
-      minValue: 1,
-      isValidAmount,
-    },
-    targetAddress: {
-      required,
-      isValidAddress,
-    },
   },
   async created() {
+    if (this.$store.state.ranking.list.length === 0) {
+      await this.$store.dispatch('ranking/update')
+    }
     this.enableWeb3 = await web3Enable(config.name)
     web3Enable(config.name)
       .then(() => {
@@ -161,30 +203,28 @@ export default {
       if (this.$v.$invalid) {
         return
       }
-      this.send()
-    },
-    setUnit(unit) {
-      this.selectedUnit = unit
+      this.nominate()
     },
     async getBalance(address) {
       const { availableBalance } = await this.api.derive.balances.all(address)
       this.tranferableBalance = new BigNumber(availableBalance)
     },
-    send() {
+    nominate() {
       this.selectedAccount = encodeAddress(this.selectedAddress, 42)
       web3FromAddress(this.selectedAccount)
         .then(async (injector) => {
           this.api.setSigner(injector.signer)
-          const amount = this.getAmount()
-          const extrinsic = await this.api.tx.balances.transfer(
-            this.targetAddress,
-            amount
+          const extrinsic = await this.api.tx.staking.nominate(
+            this.selectedAddresses
           )
           this.extrinsicHash = await extrinsic.signAndSend(this.selectedAccount)
         })
         .catch((error) => {
           console.log('Error: ', error)
         })
+    },
+    remove(accountId) {
+      this.$store.dispatch('ranking/toggleSelected', { accountId })
     },
   },
   head() {
