1099ca0724a8ef1a370dae96334065e61f188322
mariopino <info@quequiereshacer.es>
2020-11-17 10:40:48 +0100

    img folders

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
-export const config = {
-  name: 'kusama',
-  title: 'Validator resource center and ranking',
-  nodeWs: 'wss://kusama-rpc.polkadot.io',
-  denom: 'KSM',
-  addressPrefix: 2,
-  tokenDecimals: 12,
-  historySize: 28, // 1 week
-  polkascanAPI: 'https://explorer-31.polkascan.io/kusama/api/v1', // no trailing slash
-  theme: '@/assets/scss/themes/kusama.scss',
-  identiconTheme: 'polkadot',
-  logo: 'kusama-logo.svg',
-  favicon: 'kusama-favicon.ico',
-  baseURL: '/kusama-validator-resource-center/',
-}
+// export const config = {
+//   name: 'kusama',
+//   title: 'Validator resource center and ranking',
+//   nodeWs: 'wss://kusama-rpc.polkadot.io',
+//   denom: 'KSM',
+//   addressPrefix: 2,
+//   tokenDecimals: 12,
+//   historySize: 28, // 1 week
+//   polkascanAPI: 'https://explorer-31.polkascan.io/kusama/api/v1', // no trailing slash
+//   theme: '@/assets/scss/themes/kusama.scss',
+//   identiconTheme: 'polkadot',
+//   logo: 'img/logo/kusama.svg',
+//   favicon: 'img/favicon/kusama.ico',
+//   baseURL: '/kusama-validator-resource-center/',
+// }
 
 // export const config = {
 //   name: 'polkadot',
@@ -25,26 +25,26 @@ export const config = {
 //   polkascanAPI: 'https://explorer-31.polkascan.io/polkadot/api/v1', // no trailing slash
 //   theme: '@/assets/scss/themes/polkadot.scss',
 //   identiconTheme: 'polkadot',
-//   logo: 'polkadot-logo.svg',
-//   favicon: 'polkadot-favicon.ico',
+//   logo: 'img/logo/polkadot.svg',
+//   favicon: 'img/favicon/polkadot.ico',
 //   baseURL: '/kusama-validator-resource-center/',
 // }
 
-// export const config = {
-//   name: 'edgeware',
-//   title: 'Validator resource center and ranking',
-//   nodeWs: 'wss://mainnet1.edgewa.re',
-//   denom: 'EDG',
-//   addressPrefix: 7,
-//   tokenDecimals: 18,
-//   historySize: 28, // 1 week
-//   polkascanAPI: '', // no trailing slash
-//   theme: '@/assets/scss/themes/edgeware.scss',
-//   identiconTheme: 'substrate',
-//   logo: 'edgeware-logo.png',
-//   favicon: 'edgeware-favicon.ico',
-//   baseURL: '/kusama-validator-resource-center/',
-// }
+export const config = {
+  name: 'edgeware',
+  title: 'Validator resource center and ranking',
+  nodeWs: 'wss://mainnet1.edgewa.re',
+  denom: 'EDG',
+  addressPrefix: 7,
+  tokenDecimals: 18,
+  historySize: 28, // 1 week
+  polkascanAPI: '', // no trailing slash
+  theme: '@/assets/scss/themes/edgeware.scss',
+  identiconTheme: 'substrate',
+  logo: 'img/logo/edgeware.png',
+  favicon: 'img/favicon/edgeware.ico',
+  baseURL: '/kusama-validator-resource-center/',
+}
 
 // export const config = {
 //   name: 'stafi',
@@ -57,7 +57,7 @@ export const config = {
 //   polkascanAPI: '', // no trailing slash
 //   theme: '@/assets/scss/themes/stafi.scss',
 //   identiconTheme: 'substrate',
-//   logo: 'stafi-logo.png',
-//   favicon: 'stafi-favicon.ico',
+//   logo: 'img/logo/stafi.png',
+//   favicon: 'img/favicon/stafi.ico',
 //   baseURL: '/kusama-validator-resource-center/',
 // }
