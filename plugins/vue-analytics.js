e34f1e8e4dbcad252dcb3643a7b036bd9d5b8df5
mariopino <info@quequiereshacer.es>
2020-12-11 17:48:04 +0100

    send page views

--- plugins/vue-analytics.js
+++ plugins/vue-analytics.js
@@ -2,10 +2,19 @@ import Vue from 'vue'
 import VueAnalytics from 'vue-analytics'
 import { config } from '@/config.js'
 
-const kusamaValidatorsNetwork = JSON.parse(
-  decodeURIComponent(localStorage.getItem('kusamaValidatorsNetwork'))
-)
-Vue.use(VueAnalytics, {
-  id: config.googleAnalytics,
-  disabled: !kusamaValidatorsNetwork.googleAnalytics,
-})
+export default ({ app }) => {
+  const kusamaValidatorsNetwork = JSON.parse(
+    decodeURIComponent(localStorage.getItem('kusamaValidatorsNetwork'))
+  )
+  Vue.use(VueAnalytics, {
+    id: config.googleAnalytics,
+    router: app.router,
+    disabled: !kusamaValidatorsNetwork.googleAnalytics,
+    debug: {
+      enabled: true,
+    },
+    fields: {
+      cookieDomain: 'validators.kusama.network',
+    },
+  })
+}
