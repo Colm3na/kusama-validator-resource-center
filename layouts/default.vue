f26d338a4f2f26e35d5fe6a8bf21700555569a7f
mariopino <info@quequiereshacer.es>
2021-01-17 21:48:42 +0100

    misc

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,42 +1,18 @@
 <template>
   <div>
-    <HeaderKusama />
+    <Header />
     <Nuxt />
     <Footer />
   </div>
 </template>
 
 <script>
-// import { config } from '@/config.js'
-// import SelectedValidators from '@/components/SelectedValidators.vue'
-import HeaderKusama from '@/components/HeaderKusama.vue'
+import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 export default {
   components: {
-    // SelectedValidators,
-    HeaderKusama,
+    Header,
     Footer,
   },
-  // data() {
-  //   return {
-  //     config,
-  //   }
-  // },
-  // computed: {
-  //   loading() {
-  //     return this.$store.state.ranking.loading
-  //   },
-  //   selectedValidatorAddresses() {
-  //     return this.$store.state.ranking.selectedAddresses
-  //   },
-  // },
-  // watch: {
-  //   $route(to, from) {
-  //     this.$refs.selectedValidators.hide(true)
-  //   },
-  // },
-  // created() {
-  //   this.$store.dispatch('ranking/loadSelected')
-  // },
 }
 </script>
